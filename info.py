931ff2f5b083755aa2a5271337295bc3baf1951b
Adinanbotxp <132242564+Adinanbotxp@users.noreply.github.com>
2023-09-07 20:43:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = environ['API_ID', '29514095']
+API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH', 'c17fef5596d69ef2910d3f74e743eb73']
 BOT_TOKEN = environ['BOT_TOKEN', '5951735970:AAG8yX8xOZBy9f-06MnDMqcwL-Iz6VlP0XE']
 
